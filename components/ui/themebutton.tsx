dedb3a2d86f843085459cfbc8728b4b9b93a649b
watch <mahmud.alam.1011@gmail.com>
2025-07-18 11:38:59 +0600

    theme setup

---
+++ components/ui/themebutton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      className="h-8 w-8 bg-rose-100 dark:bg-rose-950"
+      variant={"ghost"}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+    </Button>
+  );
+}
