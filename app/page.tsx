3b54cda50ff441cafb8999c49923ae4fec57f15d
watch-r <nabil202051gmail.com>
2025-07-31 23:58:35 +0600

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="items-center justify-items-center min-h-screen p-8 pb-20 space-y-3.5">
-            <body className="min-h-screen flex flex-col items-center justify-center px-4 py-8">
-                {/* <!-- Game Title --> */}
-                <Image
-                    src="/img/eyespy-killer-game-horizontal.png"
-                    width={300}
-                    height={100}
-                    alt="EyeSpy: Killer Game Logo"
-                />
-                <p className="mb-6 text-gray-300 italic text-center bg-gray-800 p-4 rounded-lg">
-                    A real-time party game. Choose an option to begin:
-                </p>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            {/* <!-- Game Title --> */}
+            <Image
+                src="/img/eyespy-killer-game-horizontal.png"
+                width={300}
+                height={100}
+                alt="EyeSpy: Killer Game Logo"
+                className="mb-8 rounded-lg"
+            />
+            <p className="mb-6 text-gray-300 italic text-center p-4 rounded-lg">
+                A real-time party game. Choose an option to begin:
+            </p>
 
-                <div className="flex flex-col space-y-4">
-                    <Button className="p-3">🎮 Host Game</Button>
+            <div className="flex flex-col space-y-4">
+                <Button variant={"secondary"} size={"lg"}>
+                    🎮 Host Game
+                </Button>
+                <Button variant={"destructive"} size={"lg"}>
+                    🤝 Join Game
+                </Button>
+            </div>
+            <footer className="mt-4 text-center text-sm pt-5 rounded-lg p-3">
+                <div className="flex flex-col items-center justify-center space-y-1">
+                    <p className="text-gray-400">
+                        Made with ❤️ by MonStar Zero Studios
+                    </p>
 
-                    <Button className="p-3">🤝 Join Game</Button>
-                    <ThemeToggle />
+                    <p className="text-gray-400">© 2025 MonStar Zero Studios</p>
                 </div>
-            </body>
-            <footer className="text-sm text-gray-500">
-                © 2023 My App. All rights reserved.
             </footer>
         </div>
     );
