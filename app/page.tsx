3685f05d3886c45a8817a9b41acfa4d7b794d6af
watch-r <nabil202051gmail.com>
2025-07-31 23:46:14 +0600

    added dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <body className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center px-4 py-8">
+        <div className="items-center justify-items-center min-h-screen p-8 pb-20 space-y-3.5">
+            <body className="min-h-screen flex flex-col items-center justify-center px-4 py-8">
                 {/* <!-- Game Title --> */}
-                <img
-                    src="public/eyespy-killer-game-horizontal.png"
+                <Image
+                    src="/img/eyespy-killer-game-horizontal.png"
+                    width={300}
+                    height={100}
                     alt="EyeSpy: Killer Game Logo"
-                    className="h-30 mx-auto rounded-3xl mb-4 justify-center"
                 />
                 <p className="mb-6 text-gray-300 italic text-center bg-gray-800 p-4 rounded-lg">
                     A real-time party game. Choose an option to begin:
@@ -18,9 +20,8 @@ export default function Home() {
                 <div className="flex flex-col space-y-4">
                     <Button className="p-3">🎮 Host Game</Button>
 
-                    <button className="bg-blue-600 hover:bg-blue-700 rounded p-3 font-semibold">
-                        🤝 Join Game
-                    </button>
+                    <Button className="p-3">🤝 Join Game</Button>
+                    <ThemeToggle />
                 </div>
             </body>
             <footer className="text-sm text-gray-500">
